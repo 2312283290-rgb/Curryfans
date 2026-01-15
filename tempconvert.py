3df16fba39c31c24d64084c651e481b407c6cbd7
2312283290-rgb <2312283290@qq.com>
2026-01-15 16:49:07 +0800

    Add files via upload

---
+++ tempconvert.py
@@ -0,0 +1,12 @@
+#temconvert.py
+tempstr = input("请输入带有符号的温度值：")
+if tempstr[-1] in ["F","f"]:
+    c = (eval(tempstr[0:-1])-32)/1.8
+    print("转换后的温度是{:.2f}C".format(C))
+elif tempstr[-1] in ["C","c"]:
+        F = 1.8*eval(tempstr[0:-1]) + 32
+        print("转换后的温度是{:.2f}F".format(F))
+else:
+            print("输入格式错误")
+
+            
